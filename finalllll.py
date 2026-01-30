6f4746d3a907ee36c48c3b74e9b07c1cb7bdd06f
JIAXIC4 <JIAXIC4@NYCSTUDENTS.NET>
2026-01-30 10:17:54 -0500

    day3
    loop star

--- finalllll.py
+++ finalllll.py
@@ -4,24 +4,26 @@ t = Turtle()
 
 t.speed(0)
 
-def square(x):
+def star(x):
     t.forward(x)
-    t.left(90)
+    t.left(144)
     t.forward(x)
-    t.left(90)
+    t.left(144)
     t.forward(x)
-    t.left(90)
+    t.left(144)
     t.forward(x)
-    t.left(90)
-square(5)
+    t.left(144)
+    t.forward(x)
+    t.left(144)
+star(25)
 
-def doubleSquare(iRange):
-    length=(5)
+def doubleStar(iRange):
+    length=(25)
     for i in range(iRange):
-        square(length)
-        t.right(5)
-        length+=5
-doubleSquare(60)
+        star(length)
+        t.right(3)
+        length+=7
+doubleStar(100)
 
 turtle.done()
 
@@ -34,4 +36,4 @@ turtle.done()
 # # //Refactor the previous loop to use a function called "Square"
 # # Create a new function that uses a loop to create a triangle
 # # Write a function that draws 60 squares, turning righjt 5 degrees after each square.
-    
+# Create a function that will draw a 5 pointed star using the angle 144. Then create a function that draw a spiral of stars that looks as follows
